f3f67cbc9d19370616d76d9a6de9f1351e08163e
Resul Taşköprü <resultaskpru@gmail.com>
2021-01-02 16:34:00 +0300

    Add project files.

---
+++ dizi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] d = new int[a];
+            int b = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = r.Next(b);
+            }
+            int[] yd = new int[a];
+            for (int i = 0; i < d.Length; i++)
+            {
+                yd[yd.Length - 1 - i] = d[i];
+            }
+            foreach (int x in d)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("------------------");
+            foreach (int y in yd)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
